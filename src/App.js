293309335901b007c44ab4800c941f4c3eaaa1ef
Robert Hart <robertdhart99@gmail.com>
2023-07-10 14:43:57 -0400

    fixed the bug where handleClose was not working when adding a new budget on form submission or close

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Container from 'react-bootstrap/Container'
 import { Button, Stack } from "react-bootstrap"
-import BudgetCard from "./components/BudgetCard"
+import Container from "react-bootstrap/Container"
 import AddBudgetModal from "./components/AddBudgetModal"
 import AddExpenseModal from "./components/AddExpenseModal"
 import ViewExpensesModal from "./components/ViewExpensesModal"
+import BudgetCard from "./components/BudgetCard"
 import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
-import TotalBudgetCard from"./components/TotalBudgetCard"
+import TotalBudgetCard from "./components/TotalBudgetCard"
 import { useState } from "react"
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext"
 
@@ -22,27 +22,34 @@ function App() {
     }
 
     return (
-      <>
-        <Container className="my-4">
-          <Stack direction="horizontal" gap="2" className="mb-4">
-            <h1 className="me-auto">Budgets</h1>  
-                    <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
-                    <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
-          </Stack>
-            <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-                  gap: "1rem",
-                  alignItems: "flex-start",
-                }}
-             >
+        <>
+            <Container className="my-4">
+                <Stack direction="horizontal" gap="2" className="mb-4">
+                    <h1 className="me-auto">Budgets</h1>
+                    <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+                        Add Budget
+                    </Button>
+                    <Button variant="outline-primary" onClick={openAddExpenseModal}>
+                        Add Expense
+                    </Button>
+                </Stack>
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+                        gap: "1rem",
+                        alignItems: "flex-start",
+                    }}
+                >
                     {budgets.map(budget => {
-                        const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount,0)
+                        const amount = getBudgetExpenses(budget.id).reduce(
+                            (total, expense) => total + expense.amount,
+                            0
+                        )
                         return (
-                        <BudgetCard
+                            <BudgetCard
                                 key={budget.id}
-                                name={budget.name.name}
+                                name={budget.name}
                                 amount={amount}
                                 max={budget.max}
                                 onAddExpenseClick={() => openAddExpenseModal(budget.id)}
@@ -59,11 +66,11 @@ function App() {
                         }
                     />
                     <TotalBudgetCard />
-            </div>
-          </Container>
+                </div>
+            </Container>
             <AddBudgetModal
                 show={showAddBudgetModal}
-                handleCLose={() => setShowAddBudgetModal(false)}
+                handleClose={() => setShowAddBudgetModal(false)}
             />
             <AddExpenseModal
                 show={showAddExpenseModal}
@@ -74,8 +81,8 @@ function App() {
                 budgetId={viewExpensesModalBudgetId}
                 handleClose={() => setViewExpensesModalBudgetId()}
             />
-       </>
+        </>
     )
 }
 
-export default App;
+export default App
